ca65dcad575bbe0d7ecc7d2969af2e25f02be697
Ivan <70635146+themilchenko@users.noreply.github.com>
2021-03-18 22:28:14 +0300

    Update SchoolRegion.cpp

--- SchoolRegion.cpp
+++ SchoolRegion.cpp
@@ -1,12 +1,12 @@
 #include "SchoolRegion.h"
 
-double SchoolRegion::get_finance_all_school()
+double SchoolRegion::finance_all_school()
 {
-    return finance_all_school() * odds;
+    return School::finance_all_school() * odds;
 }
 
 void SchoolRegion::print()
 {
-    print_school();
-    print_region();
+    Region::print();
+    School::print();
 }

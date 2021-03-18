be77c3edd78734dc641675aa6d7343510f12ce65
Ivan <70635146+themilchenko@users.noreply.github.com>
2021-03-18 22:28:38 +0300

    Update SchoolRegion.h

--- SchoolRegion.h
+++ SchoolRegion.h
@@ -10,10 +10,12 @@ class SchoolRegion final : public School, public Region
 {
 
 public:
-    SchoolRegion(const std::string& school, const int& finances, const int& size, const std::string& region,
-                 const double& odd) : School(school, finances, size), Region(region, odd) { };
+    SchoolRegion(const std::string& school, const int& finances, const int& size, const std::string& region, const double& odd) : 
+        School(school, finances, size),
+        Region(region, odd) 
+    { };
 
-    double get_finance_all_school();
+    double finance_all_school();
 
     void print();
 

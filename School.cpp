6d5db9fe7ee6d8a648a07dd72d525c96bbc918ad
Ivan <70635146+themilchenko@users.noreply.github.com>
2021-03-18 22:27:04 +0300

    Update School.cpp

--- School.cpp
+++ School.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
 #include "School.h"
 
-School::School(const std::string& name_of_school, const int& finances_on_1_teacher, const int& size_of_teachers)
+School::School(const std::string& name, const int& finances, const int& size)
 {
-    this->name_of_school = name_of_school;
-    this->finances_on_1_teacher = finances_on_1_teacher;
-    this->size_of_teachers = size_of_teachers;
+    name_of_school = name;
+    finances_on_1_teacher = finances;
+    size_of_teachers = size;
 }
 
-School::~School()
-{
-    this->name_of_school = "";
-    this->finances_on_1_teacher = 0;
-    this->size_of_teachers = 0;
-}
-
-void School::print_school()
+void School::print()
 {
     std::cout << "School: " << name_of_school << " Finances on 1 teacher: " << finances_on_1_teacher <<
-              " Size of teachers: " << size_of_teachers << std::endl;
+        " Size of teachers: " << size_of_teachers << std::endl;
 }
 
 int School::finance_all_school()
 {
-    return this->size_of_teachers * finances_on_1_teacher;
+    return size_of_teachers * finances_on_1_teacher;
 }
